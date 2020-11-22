ff606bf40d3ecd24fd220cfe79e94080ded4e264
Vikrant Singh Bandral <vikrant.immeasurable@gmail.com>
2020-11-22 17:11:04 +0530

    Added index.js with Redux setup

---
+++ index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Routes, store } from './routes';
+import { signIn } from './actions';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import '../styles/social.css';
+import 'jquery';
+
+const token = localStorage.getItem('token');
+if (token) {
+	store.dispatch(signIn());
+}
+
+ReactDOM.render(
+	<Provider store={store}>
+		<Routes />
+	</Provider>,
+	document.getElementById('root')
+);
