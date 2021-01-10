15f552acad84004770a6b619c257cf8bf435f9ed
Vikrant Singh Bandral <vikrant.immeasurable@gmail.com>
2021-01-11 03:30:39 +0530

    Changed host Url

--- client/config.js
+++ client/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    hostUrl: 'http://localhost:5000',
+    hostUrl: 'https://ancient-wave-27498.herokuapp.com',
     clientUrl: 'http://localhost:3000',
     database: 'mongodb://localhost:27017/iStudy',
     secret: "$S$E$C$R$E$T$",
