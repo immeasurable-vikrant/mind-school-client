cb509cbb00935b50b7952cb17517afce76ce613d
Vikrant Singh Bandral <vikrant.immeasurable@gmail.com>
2021-01-12 03:46:34 +0530

    Added Client url to the deployed url

--- client/config.js
+++ client/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     hostUrl: 'https://ancient-wave-27498.herokuapp.com',
-    clientUrl: 'http://localhost:3000',
+    clientUrl: 'https://immeasurable.netlify.app/',
     database: 'mongodb://localhost:27017/iStudy',
     secret: "$S$E$C$R$E$T$",
     port: 5000
