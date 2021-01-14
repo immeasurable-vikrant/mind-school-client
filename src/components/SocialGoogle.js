158d5be06e4c0816532381d8bc71f59f3cf3fda9
Vikrant Singh Bandral <vikrant.immeasurable@gmail.com>
2021-01-14 14:39:26 +0530

    Fixed token in Social-google, token setting in localstorage

--- src/components/SocialGoogle.js
+++ src/components/SocialGoogle.js
@@ -1,16 +1,21 @@
+/** @format */
+
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter, useHistory, useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { signIn, userInfo } from '../actions';
 
-const SocialGoogle = ({ fetchSignIn, fetchUserInfo }) => {
+const SocialGoogle = ({ fetchSignIn, fetchUserInfo, location }) => {
   const history = useHistory();
-
-  const { token } = useParams();
+  // console.log('match', match)
+  // const { token } = useParams();
 
   useEffect(() => {
-    localStorage.setItem('token', token);
+    console.log('location', location.search)
+    const params = new URLSearchParams(location.search)
+    const setToken = params.get('token')
+    localStorage.setItem('token',setToken)
     fetchSignIn();
     fetchUserInfo();
     history.push('/');
@@ -26,13 +31,13 @@ const SocialGoogle = ({ fetchSignIn, fetchUserInfo }) => {
 SocialGoogle.propTypes = {
   location: PropTypes.object,
   fetchUserInfo: PropTypes.func,
-  fetchSignIn: PropTypes.func
+  fetchSignIn: PropTypes.func,
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchSignIn: () => dispatch(signIn()),
-    fetchUserInfo: () => dispatch(userInfo())
+    fetchUserInfo: () => dispatch(userInfo()),
   };
 };
 
